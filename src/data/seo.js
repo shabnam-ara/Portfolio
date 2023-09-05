9ff6b8c458f16d42007e947dd43d8a2ef5d46f15
Shabnam Ara <137683107+shabnam-ara@users.noreply.github.com>
2023-09-05 06:51:54 -0900

    Add files via upload

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"",
+		keywords: ["Shabnam"],
+	},
+
+	{
+		page: "about",
+		description:
+			"",
+		keywords: ["Shabnam "],
+	},
+
+	{
+		page: "articles",
+		description:
+			"",
+		keywords: ["Shabnam"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"",
+		keywords: ["Shabnam"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"",
+		keywords: ["Shabnam"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,89 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Shabnam",
+		name: "Shabnam Ara.",
+		email: "shabnamara44953@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/Shabnam_Ara1",
+		github: "https://github.com/shabnam-ara",
+		linkedin: "https://www.linkedin.com/in/shabnam-ara-747658280/",
+		instagram: "https://www.instagram.com/graphics_design786/",
+
+	},
+
+	homepage: {
+		title: "Frontend Developer & Graphic Designer.",
+		description:
+			"My name is Shabnam Ara, and I am from Ghaziabad. I received my early education at Government Girls Senior Secondary School in Vivek Vihar, Delhi. My academic journey continued as I pursued a Bachelor of Computer Applications (BCA) degree from CCSU (Chaudhary Charan Singh University) and later achieved a Master's degree in Computer Applications (MCA) from AKTU (Dr. A.P.J. Abdul Kalam Technical University). In terms of technical skills, I possess proficiency in a range of web development technologies, including HTML, CSS, JavaScript, and ReactJS. I've had the opportunity to apply these skills in various projects, such as creating login and registration pages, developing a GYM website, crafting an Amazon clone, and designing user-friendly signup pages.",
+	},
+
+	about: {
+		title: "I’m Shabnam Ara. I live in Ghaziabad, where I design Websites.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	projects: [
+		{
+			title: "Quiz Game",
+			description:
+				"Introducing our engaging and interactive quiz game, meticulously crafted using the power of HTML, CSS, and JavaScript!.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://quiz-game-liard.vercel.app/",
+		},
+
+		{
+			title: "Amazon Clone",
+			description:
+				"Creating a simple Amazon clone user interface with HTML and CSS involves several steps and components to mimic the look and feel of the Amazon website",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://amazon-clone-olive-eight.vercel.app/",
+		},
+
+		{
+			title: "SignUp Page",
+			description:
+				"this project name is sign up page you can register yourself in this page. also incorporates captivating animations to enhance the overall user interface",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://sign-up-sigma-five.vercel.app/",
+		},
+		
+
+		{
+			title: "Weather Report",
+			description:
+				"Creating a weather website with the ability to check the temperature of any place by entering its name is a great project. Here's a high-level outline of how you can build it using HTML, CSS, JavaScript, and React.js.",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+			linkText: "View Project",
+			link: "https://weather-api-2l6x.vercel.app/",
+		},
+		{
+			title: "Gym Website",
+			description:
+				"Creating a user-friendly and visually appealing gym website user interface involves combining HTML and CSS to design and style the website elements. Below, I'll provide you with a simple example of a gym website UI using HTML and CSS.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://gym-landing-page-blond.vercel.app/",
+			
+		},
+		
+		{
+			title: "CRUD",
+			description:
+				"Creating a CRUD (Create, Read, Update, Delete) application with React is a common project for web developers. you'll build a user interface that allows users to perform basic operations on a set of data.",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+			linkText: "View Project",
+			link: "https://crud-puce-sigma.vercel.app/",
+		},
+		
+
+	],
+};
+
+export default INFO;
